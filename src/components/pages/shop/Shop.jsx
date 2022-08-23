138abcac10e13e62ac01063172cc62c2920c7ab0
Shipalan <shipalan678@gmail.com>
2022-08-23 12:38:10 -0600

    adding products to page

--- src/components/pages/shop/Shop.jsx
+++ src/components/pages/shop/Shop.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import "./Shop.css";
+const pic = "../../../images/Products"
 const axios = require("axios");
 
 const Shop = () => {
@@ -10,20 +11,34 @@ const Shop = () => {
       .get("http://localhost:4000/api/allProducts")
       .then((res) => {
         setProducts(res.data);
+        console.log(products)
+        console.log(res.data)
       })
       .catch((err) => {
-        console.log("Error here");
+        console.log("ID-10-t error");
         console.log(err);
       });
   };
 
   useEffect(() => {
     getProducts();
-    console.log(products)
   }, []);
-  return <div>
-    {products.map((e,i) => <p key={i}>{JSON.stringify(e)}</p>)}
-  </div>;
+  console.log()
+
+  return (
+    <div>
+      {products.length > 0 &&
+      products.map((p, i) => {
+        return (
+          <div key={i}>
+            <img src={require(`${pic}/${p.product_img}`)} alt="Product" />
+            <p>{p.product_price}</p>
+            <p>{p.product_description}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Shop;
