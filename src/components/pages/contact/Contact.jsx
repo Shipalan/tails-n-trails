ed3f3412796a8dc69b566e239efcb91a6c8e3850
Shipalan <shipalan678@gmail.com>
2022-09-19 14:04:45 -0600

    Added the contact page

--- src/components/pages/contact/Contact.jsx
+++ src/components/pages/contact/Contact.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import location from "../../../images/References/Location.png";
+import './Contact.css'
 
 const Contact = () => {
   return (
-    <p>Contact</p>
-  )
-}
+    <div className="contactGroup">
+      <h3 className="contactTitle">Contact Us 😁</h3>
+      <section className="contactSection">
+        <img src={location} alt="map" />
+        <article>
+          <p className="contact-instruction">
+            If you have any questions or concerns please reach out to us at one
+            of the following.
+          </p>
+          <p>Phone Number: 123-456-7890</p>
+          <p>Email Address: tails&trails@gmail.com</p>
+        </article>
+      </section>
+    </div>
+  );
+};
 
-export default Contact
+export default Contact;
